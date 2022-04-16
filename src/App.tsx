e36ca37c99630ccd7e4934c724b00c85cbc89f04
Anatoliy11111 <canatolij@list.ru>
2022-04-17 02:13:40 +0300

    fix bag reboot page card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
 
 import style from 'App.module.scss';
 import { Preloader } from 'Component/01-common/preloader/Preloader';
-import { Login } from 'Component/02-auth';
 import { NavigateMenu, RouteMenu } from 'Component/06-navigate';
 import { RootState } from 'redux/store/Store';
 import { setNameTC } from 'redux/thunk';
